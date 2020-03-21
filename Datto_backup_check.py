a54545525701ec9d35b732055071291eb1041129
Tommy Harris <tomharris89@gmail.com>
2020-03-21 17:41:19 -0600

    Add missing table field in Verification

--- Datto_backup_check.py
+++ Datto_backup_check.py
@@ -157,7 +157,7 @@ def buildEmailBody(results_data):
         MSG_BODY += '<h1>Local Verification Issues</h1><table>\
         <tr><th>Appliance</th><th>Agent</th><th>Error Type</th><th>Error Message</th></tr>'
         for error in results_data['verification_error']:
-            MSG_BODY += '<tr><td>' + error[1] + '</td><td>' + error[2] + '</td><td>' + error[3] + '</td></tr>'
+            MSG_BODY += '<tr><td>' + error[1] + '</td><td>' + error[2] + '</td><td>' + error[3] + '</td><td>' + error[4] + '</td></tr>'
         MSG_BODY += '</table>'
 
     if results_data['informational']:
