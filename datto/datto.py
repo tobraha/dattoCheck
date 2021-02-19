9a09c4e82256dda01ee3bbe1524e2392a9e213b5
Tommy Harris <tobraha@tilde.team>
2021-02-19 07:26:47 -0700

    Remove unused mail import; add html report screenshot

--- datto/datto.py
+++ datto/datto.py
@@ -238,7 +238,6 @@ class DattoCheck():
         """
 
         import smtplib
-        from email.mime.application import MIMEApplication
         from email.mime.multipart import MIMEMultipart
         from email.mime.text import MIMEText
 
